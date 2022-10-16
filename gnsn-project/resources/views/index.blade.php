403c6fd8c3bbc290e1cbbb0123557e300dd8f2b0
yuc <sy97r413@gmail.com>
2022-10-16 17:29:26 +0900

    cssファイル追加、プルダウンの長さ調整

--- gnsn-project/resources/views/index.blade.php
+++ gnsn-project/resources/views/index.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="/css/style.css" >
 <select class = "selectPrayers" id = "NormalPrayers_id">  
   @foreach($NormalPrayers as $NormalPrayer)
     <option value="{{ $NormalPrayer->id }}">{{ $NormalPrayer->name }}</option>
