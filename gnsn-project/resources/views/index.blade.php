18a49c43210597a3874ff87e693ab6806220e829
yuc <sy97r413@gmail.com>
2022-10-16 17:12:45 +0900

    プルダウン

--- gnsn-project/resources/views/index.blade.php
+++ gnsn-project/resources/views/index.blade.php
@@ -1,3 +1,5 @@
-@foreach($NormalPrayers as $NormalPrayer)
-  {{$NormalPrayer->name}}
-@endforeach
+<select class = "selectPrayers" id = "NormalPrayers_id">  
+  @foreach($NormalPrayers as $NormalPrayer)
+    <option value="{{ $NormalPrayer->id }}">{{ $NormalPrayer->name }}</option>
+  @endforeach
+</select>
